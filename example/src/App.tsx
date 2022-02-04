b8a957e8b4e64351d1bcd92349d49b9fa6891c61
marco <marcon@hyperting.com>
2022-02-04 15:08:13 +0100

    fix: ifram pointer events in example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -6,6 +6,7 @@ import '@fontsource/sora'
 import { ChakraProvider, theme } from '@chakra-ui/react'
 import { Route } from 'react-router-dom'
 import { ExampleStrategy } from './ExampleStrategy'
+import './App.css'
 
 export const App = () => {
   return (
