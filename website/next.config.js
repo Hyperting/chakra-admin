f3685d5118a0cd7609a5850da9f60bf834e9ce5d
Marco Nalon <marcon@hyperting.com>
2022-10-26 14:22:21 +0200

    chore: removing old wrong files

--- website/next.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-const compose = require('compose-function')
-const { withDokz } = require('dokz/dist/plugin')
-
-const composed = compose(withDokz)
-
-module.exports = composed({
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-})
